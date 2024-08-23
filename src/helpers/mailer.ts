16e03bb4941d67dcd9daf82447d60f7c17a3b562
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-24 00:43:42 +0600

    Update : register user and verify email method created

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,14 +1,29 @@
+import { User } from '@/models/userModel';
 import nodemailer from 'nodemailer';
+import bcrypt from 'bcryptjs';
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
+    const hashedToken = await bcrypt.hash(userId.toString(), 10);
+
+    if (emailType === 'VERIFY') {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === 'RESET') {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
     const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // Use `true` for port 465, `false` for all other ports
+      host: 'sandbox.smtp.mailtrap.io',
+      port: 2525,
       auth: {
-        user: 'maddison53@ethereal.email',
-        pass: 'jn7jnAPss4f63QBp6D',
+        user: '6582774b8c5d1a', // .env
+        pass: 'd76d574fe0039e', // .env
       },
     });
 
@@ -17,7 +32,12 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email, // list of receivers
       subject: emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password', // Subject line
       //   text: 'Hello world?', // plain text body
-      html: '<b>Hello world?</b>', // html body
+      html: `<a href="${process.env.DOMAIN}/verifyEmail?token=${hashedToken}">here</a> to
+       ${emailType === 'VERIFY' ? 'verify your email' : ' reset your password'}
+      or copy and paste the link below in your browser.
+      <br>
+      ${process.env.DOMAIN}/verifyEmail?token=${hashedToken}
+      </p>`,
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
