8d7c0154b4532f0f78209e9c5c956b2cb0dd6937
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-25 00:14:27 +0600

    Update: verify email and login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,75 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
-function page() {
-  return <div>page</div>;
+function LoginPage() {
+  const router = useRouter();
+  const [user, setUsr] = useState({
+    email: '',
+    password: '',
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post(`/api/users/login`, user);
+      console.log('login success', response.data);
+      router.push('/profile');
+    } catch (error: any) {
+      console.log('signup failed');
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>{loading ? 'Processing' : 'Login'}</h1>
+      <hr />
+      <label htmlFor='email'>Email</label>
+      <input
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+        type='email'
+        id='email'
+        value={user.email}
+        onChange={(e) => {
+          setUsr({ ...user, email: e.target.value });
+        }}
+        placeholder='email'
+      />
+      <label htmlFor='password'>Password</label>
+      <input
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+        type='password'
+        id='password'
+        value={user.password}
+        onChange={(e) => {
+          setUsr({ ...user, password: e.target.value });
+        }}
+        placeholder='password'
+      />
+      <button
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        onClick={onLogin}
+      >
+        {buttonDisabled ? 'No Login' : 'Login'}
+      </button>
+
+      <Link href={'/signup'}>Visit Sign up Page</Link>
+    </div>
+  );
 }
 
-export default page;
+export default LoginPage;
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 import axios from 'axios';
-import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 export default function VerifyEmailPage() {
+  //   const router = useRouter();
+
   const [token, setToken] = useState('');
   const [verified, setVerified] = useState(false);
   const [error, setError] = useState(false);
@@ -11,11 +15,45 @@ export default function VerifyEmailPage() {
     try {
       await axios.post('/api/users/verifyemail', { token });
       setVerified(true);
+      setError(false);
     } catch (error: any) {
       setError(error);
       console.log(error.response.data);
     }
   };
 
-  return <div>verifyEmail</div>;
+  useEffect(() => {
+    setError(false);
+    const urlToken = window.location.search.split('=')[1];
+    setToken(urlToken || '');
+
+    // const {query} = router;
+    // const urlToken2 =  query.token;
+    // setToken(urlToken2);
+  }, []);
+
+  useEffect(() => {
+    setError(false);
+    if (token.length > 0) {
+      verifyUserEmail();
+    }
+  }, [token]);
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1 className='text-xl'>Verify Email</h1>
+      <h2 className='p-2 bg-green-400'>{token ? `${token}` : 'no token'}</h2>
+      {verified && (
+        <div>
+          <h2>Verified</h2>
+          <Link href={'/login'}>Login</Link>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2>Error</h2>
+        </div>
+      )}
+    </div>
+  );
 }
